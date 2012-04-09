8a23d338a1d05e0d8a80e74adffefb35a2ba0a9d
westwooder <sz31654270@gmail.com>
2012-04-09 17:50:33 +0800

    init version

---
+++ MagicUpdate/frmComputeHash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MagicUpdate
+{
+    public partial class frmComputeHash : Form
+    {
+        public frmComputeHash()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            string file=tbxFile.Text;
+            if (!string.IsNullOrEmpty(file))
+            {
+                if (File.Exists(file))
+                {
+                    tbxMd5.Text = Core.GetHash(file);
+                }
+            }
+        }
+    }
+}
