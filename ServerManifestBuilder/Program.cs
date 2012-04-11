c77d43c4996b910882e4162c06aebe35e6a74344
westwooder <sz31654270@gmail.com>
2012-04-11 17:28:09 +0800

    ETST3

--- ServerManifestBuilder/Program.cs
+++ ServerManifestBuilder/Program.cs
@@ -10,7 +10,7 @@ namespace ServerManifestBuilder
         /// 应用程序的主入口点。 
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main() 
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
